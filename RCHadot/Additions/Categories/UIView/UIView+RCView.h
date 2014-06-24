710852e149816fb30f1bc29f25402ca6b8e6b21e
Looping <www.looping@gmail.com>
2014-06-24 10:58:01 +0800

    Fix typo

--- RCHadot/Additions/Categories/UIView/UIView+RCView.h
+++ RCHadot/Additions/Categories/UIView/UIView+RCView.h
@@ -3,7 +3,7 @@
 //  RCHadot
 //
 //  Created by Looping on 14/6/24.
-//  Copyright (c) 2014年 RidgeCorn. All rights reserved.
+//  Copyright (c) 2014    RidgeCorn. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
